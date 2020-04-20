6744ec1d27836896cd9cdff4dfcda135a38060e7
Duke <duke.raphael.ng@gmail.com>
2020-04-20 13:33:49 +1000

    Routes: shorten signin and signup routes, redirect authenticated and unauthenticated users differently, remove index.html by changing name

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'home#index'
+  devise_for :users
+  # root to: 'home#index'
+
+  devise_scope :user do
+    get 'signin', to: 'devise/sessions#new'
+    get 'signup', to: 'devise/registrations#new'
+
+    authenticated :user do
+      root 'home#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+
+    # For more details: https://stackoverflow.com/questions/4954876/setting-devise-login-to-be-root-page
+  end
 end
